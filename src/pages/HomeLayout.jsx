ce1795ac5108aed562332964dc5cf7befa3d18e6
bassem <basem12683@gmail.com>
2024-03-21 05:27:43 +0300

    Loading

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Outlet, useNavigation } from "react-router-dom";
-import { Header, Navbar } from "../components";
+import { Header, Loading, Navbar } from "../components";
 
 const HomeLayout = () => {
   const navigation = useNavigation();
-  const isPageLoading = navigation.state === 'loading';
+  const isPageLoading = navigation.state === "loading";
   return (
     <div>
-      <Header/>
-      <Navbar/>
-      <div className="container">
-        <Outlet />
-      </div>
+      <Header />
+      <Navbar />
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <div className="container">
+          <Outlet />
+        </div>
+      )}
     </div>
   );
 };
