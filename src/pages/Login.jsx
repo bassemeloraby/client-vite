3a468792ad3fa5067b20c07aea15921995150d5b
bassem <basem12683@gmail.com>
2024-03-21 04:55:51 +0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -60,49 +60,53 @@ const Login = () => {
 
   return (
     <Fragment>
-    <section
-      className="justify-content-md-center"
-      style={{ display: "grid" }}
-    >
-      <h1>
-        <FaSignInAlt /> Login
-      </h1>
-      <p>Login and start settings</p>
-    </section>
-    <section className="form col-4 p-2 mb-2" style={{backgroundColor:"brown"}}>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={email}
-            placeholder="Enter your email"
-            onChange={onChange}
-          />
+      <div
+        className=" justify-content-center text-center"
+        style={{ display: "grid", paddingTop: "20%" }}
+      >
+        <div>
+          <h1>
+            <FaSignInAlt /> Login
+          </h1>
+          <p>Login and start settings</p>
         </div>
-        <div className="mt-2">
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            value={password}
-            placeholder="Enter password"
-            onChange={onChange}
-          />
-        </div>
+        <div style={{ backgroundColor: "brown" }} className="p-2 ">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={onChange}
+              />
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="Enter password"
+                onChange={onChange}
+              />
+            </div>
 
-        <div className="mt-2">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
+            <div className="mt-2">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-    </section>
-    {wrongMessage && <h3 className="text-danger">wrong email or password ... try again </h3>}
-  </Fragment>
+      </div>
+      {wrongMessage && (
+        <h3 className="text-danger">wrong email or password ... try again </h3>
+      )}
+    </Fragment>
   );
 };
 
