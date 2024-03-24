1059da159a1a0f5296a9651649fddc9db1411794
bassem <basem12683@gmail.com>
2024-03-24 11:32:54 +0300

    customFetch

--- src/features/drugs/drugsService.js
+++ src/features/drugs/drugsService.js
@@ -1,19 +1,23 @@
 import axios from 'axios';
+import { customFetch } from "../../utils/index";
 
 import { mainUrl } from "../../utils/Data";
+const url = "allDrugs";
 
 
 const API_URL = mainUrl + "allDrugs";
 
 const getDrugs = async () => {
-    const response = await axios.get(API_URL);
+    // const response = await axios.get(API_URL);
+    const response = await customFetch(url);
   
     return response.data;
   };
 
 
 const getOneDrug = async (id) => {
-    const response = await axios.get(`${API_URL}/${id}`);
+    // const response = await axios.get(`${API_URL}/${id}`);
+    const response = await customFetch(`${url}/${id}`);
   console.log(response)
     return response.data;
   };
